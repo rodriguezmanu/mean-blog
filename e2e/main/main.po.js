05239b1b25b022020bce6cf5f3c1883984b7cc79
Manuel Rodriguez <rodriguez.manu@gmail.com>
2015-12-29 22:35:11 -0300

    Fix automation test

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -9,11 +9,6 @@ var MainPage = function() {
     this.heroEl = element(by.css('.hero-unit'));
     this.h1El = this.heroEl.element(by.css('h1'));
     this.imgEl = this.heroEl.element(by.css('img'));
-
-    this.panelFixedEl = element(by.css('ng-cta-fixed .panel'));
-    this.panelFixedHrefEl = this.panelFixedEl.element(by.css('a'));
-    this.panelFixedDescription = this.panelFixedHrefEl.element(by.css('.description-text'));
-    this.panelFixedCta = this.panelFixedHrefEl.element(by.css('.cta-text'));
 };
 
 module.exports = new MainPage();
